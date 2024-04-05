c5e4eaf105d73da6da26c53696eccdd77e0fd73b
mahesh <maheshghamand@gmail.com>
2024-04-05 12:58:19 +0530

    store last timer history

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var timerFileDir = os.Getenv("HOME") + "/.timer1/"
-var timerFile = timerFileDir + "timer.txt"
+const (
+	TimerFileDir = "/.timer1/"
+	TimerFile    = "timer.txt"
+	FileMode     = 0755
+)
+
+var timerFileDir = os.Getenv("HOME") + TimerFileDir
+var timerFile = timerFileDir + TimerFile
 
 var rootCmd = &cobra.Command{
 	Use:   "Timer1",
@@ -23,57 +29,79 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-var startCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Start a timer for a project",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if _, err := os.Stat(timerFile); err == nil {
-			fmt.Println("A timer is already running; Please stop the current timer before starting a new one.")
-			return
-		}
-		projectName := args[0]
-		startTime := time.Now().Unix()
-		data := fmt.Sprintf("%s\n%d", projectName, startTime)
-		// os.MkdirAll creates a directory named timerFileDir, along with any necessary parents.
-		// The second argument, 0600, sets the file mode: owner can read and write,
-		//   but the file is not executable and not accessible by others.
-		// 0600 is an octal number, each digit represents the permission for the owner, group, and others,
-		//   in that order.
-		// leading 0 indicates an octal number.
-		// 6 is the sum of 4 (read) and 2 (write), so 0600 means read and write for the owner.
-		err := os.MkdirAll(timerFileDir, 0600)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		err = os.WriteFile(timerFile, []byte(data), 0600)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("Started timer for project: %s\n", projectName)
-	},
+func writeToFile(data string) {
+	err := os.WriteFile(timerFile, []byte(data), FileMode)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		os.Exit(1)
+	}
 }
 
-func timerDetails() (*string, int, int) {
+func startTimer(projectName string) {
+	data := fmt.Sprintf("%s\n%d\n%d", projectName, time.Now().Unix(), 0)
+	writeToFile(data)
+	fmt.Printf("Started timer for project: %s\n", projectName)
+}
+
+func timerDetails() (*string, int, int, bool, int64, int64) {
 	data, err := os.ReadFile(timerFile)
 	if err != nil {
 		fmt.Println("No timer is currently running.")
-		return nil, 0, 0
+		os.Exit(1)
 	}
 	lines := strings.Split(string(data), "\n")
 	projectName := lines[0]
 	startTime, err := strconv.ParseInt(lines[1], 10, 64)
 	if err != nil {
 		fmt.Printf("Error parsing timer info: %v\nStop current timer to remove the error\n", err)
-		return nil, 0, 0
+		return nil, 0, 0, false, 0, 0
 	}
+	stopTime, err := strconv.ParseInt(lines[2], 10, 64)
+	if err != nil {
+		fmt.Printf("Error parsing timer info: %v\nStop current timer to remove the error\n", err)
+	}
+	isStopped := stopTime != 0
+
 	elapsed := time.Since(time.Unix(startTime, 0))
+	if isStopped {
+		elapsed = time.Unix(stopTime, 0).Sub(time.Unix(startTime, 0))
+	}
 	elapsedMinutes := int(elapsed.Minutes())
 	elapsedSeconds := int(elapsed.Seconds()) % 60 // remaining seconds after minutes
 
-	return &projectName, elapsedMinutes, elapsedSeconds
+	return &projectName, elapsedMinutes, elapsedSeconds, isStopped, startTime, stopTime
+}
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start a timer for a project",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// make timerFile if not already exists
+		if _, err := os.Stat(timerFileDir); os.IsNotExist(err) {
+			err := os.MkdirAll(timerFileDir, FileMode)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			_, err = os.Create(timerFile)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			startTimer(args[0])
+		} else {
+			projectName, _, _, isStopped, _, _ := timerDetails()
+			if !isStopped {
+				fmt.Printf("A timer is already running for project: %s\n", *projectName)
+				return
+			} else {
+				startTimer(args[0])
+			}
+		}
+	},
 }
 
 var listCmd = &cobra.Command{
@@ -81,8 +109,11 @@ var listCmd = &cobra.Command{
 	Short: "List currently running timer",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		projectName, elapsedMinutes, elapsedSeconds := timerDetails()
+		projectName, elapsedMinutes, elapsedSeconds, isStopped, _, _ := timerDetails()
 		if projectName != nil {
+			if isStopped {
+				println("Timer is stopped")
+			}
 			details := fmt.Sprintf("Project: %s, elapsed time: %d minutes %d seconds \n", *projectName, elapsedMinutes, elapsedSeconds)
 			println(details)
 		}
@@ -94,13 +125,19 @@ var stopCmd = &cobra.Command{
 	Short: "Stop currently running timer",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		projectName, elapsedMinutes, elapsedSeconds := timerDetails()
+		projectName, elapsedMinutes, elapsedSeconds, isStopped, startTime, _ := timerDetails()
 		if projectName != nil {
+			if isStopped {
+				println("No Timer is running currently.")
+				return
+			}
 			details := fmt.Sprintf("Project: %s, elapsed time: %d minutes %d seconds \n", *projectName, elapsedMinutes, elapsedSeconds)
 			println(details)
 			println("Succesfully stopped project:", *projectName)
 		}
-		os.Remove(timerFile)
+		// stop the timer by adding stop time at 3rd line of the file
+		data := fmt.Sprintf("%s\n%d\n%d", *projectName, startTime, time.Now().Unix())
+		writeToFile(data)
 	},
 }
 
