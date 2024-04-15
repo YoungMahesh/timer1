a1679ac0e108b274b4a47fb167f2679afe22ab6e
mahesh <maheshghamand@gmail.com>
2024-04-15 14:06:50 +0530

    edit minutes of last session of project

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -49,7 +50,7 @@ var startCmd = &cobra.Command{
 			}
 
 			saveTimerData(args[0], sessions)
-			println("started project: ", args[0])
+			println("\nstarted project: ", args[0])
 		} else {
 			projectName, _, isStopped := readTimer()
 			if !isStopped {
@@ -70,18 +71,64 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		projectName, sessions, isStopped := readTimer()
 
-		if projectName != nil {
-			status := "Running"
-			if isStopped {
-				status = "Stopped"
-			}
-			title := fmt.Sprintf("Project: %s; Status: %s\n", *projectName, status)
-			println(title)
-			printSessions(sessions)
-
-		} else {
+		if projectName == nil {
 			println("No timer is currently running.")
+			return
 		}
+
+		status := "Running"
+		if isStopped {
+			status = "Stopped"
+		}
+		title := fmt.Sprintf("\nProject: %s; Status: %s\n", *projectName, status)
+		println(title)
+		printSessions(sessions)
+	},
+}
+
+var editCmd = &cobra.Command{
+	Use:                "edit",
+	Short:              "Edit time for the project",
+	Args:               cobra.ExactArgs(1),
+	DisableFlagParsing: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		errMsg1 := "Please provide number of minutes to add(e.g. 31) or remove(e.g. -20) from last session."
+		if len(args) < 1 {
+			println(errMsg1)
+			return
+		}
+		editInfo := args[0]
+		if len(editInfo) < 2 {
+			println(errMsg1)
+			return
+		}
+		minutes, err := strconv.Atoi(editInfo)
+		if err != nil {
+			println(errMsg1)
+			return
+		}
+		projectName, sessions, _ := readTimer()
+		if projectName == nil {
+			println("No timer is currently running.")
+			return
+		}
+		if len(sessions) == 0 {
+			println("No sessions found.")
+			return
+		}
+
+		lastSession := sessions[len(sessions)-1]
+		if minutes < 0 {
+			// add negative minutes from endTime to remove time
+			lastSession[1] += int64(minutes * 60)
+		} else {
+			// remove positive minutes from startTime to add time
+			lastSession[0] -= int64(minutes * 60)
+		}
+		sessions[len(sessions)-1] = lastSession
+		saveTimerData(*projectName, sessions)
+		println("\nedited project:", *projectName, "\n")
+		printSessions(sessions)
 	},
 }
 
@@ -91,18 +138,15 @@ var stopCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		projectName, sessions, isStopped := readTimer()
-		if projectName != nil {
-			if isStopped {
-				println("No Timer is running currently.")
-				return
-			}
-			printSessions(sessions)
-			println("\nStopped project:", *projectName)
+		if projectName == nil || isStopped {
+			println("No timer is currently running.")
+			return
 		}
-
 		// stop timer by adding stop time to last session
 		sessions[len(sessions)-1][1] = time.Now().Unix()
 		saveTimerData(*projectName, sessions)
+		println("\nstopped project:", *projectName, "\n")
+		printSessions(sessions)
 	},
 }
 
@@ -112,24 +156,27 @@ var restartCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		projectName, sessions, isStopped := readTimer()
-		if projectName != nil {
-			printSessions(sessions)
-			if !isStopped {
-				println("Timer is already running for project:", *projectName)
-				return
-			}
+		if projectName == nil {
+			println("No timer is currently running.")
+			return
+		}
+		if !isStopped {
+			println("Timer is already running for project:", *projectName)
+			return
 		}
 
 		// restart timer by adding new session
 		sessions = append(sessions, [2]int64{time.Now().Unix(), 0})
 		saveTimerData(*projectName, sessions)
-		println("\nRestarted project:", *projectName)
+		println("\nrestarted project:", *projectName, "\n")
+		printSessions(sessions)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
 	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(editCmd)
 	rootCmd.AddCommand(stopCmd)
 	rootCmd.AddCommand(restartCmd)
 }
